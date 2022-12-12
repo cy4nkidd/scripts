8b8e20d89e7d6d3e3683cc8823963d276f8aa3c9
cy4nkidd <117698747+cy4nkidd@users.noreply.github.com>
2022-12-11 20:29:50 -0600

    Update lol.lua

--- lol.lua
+++ lol.lua
@@ -15,7 +15,7 @@ video.Size = UDim2.new(1,0,1,0)
 local Name = "Peter+Griffin+Says+The+N+Word.mp4"
 
 if not isfile(Name) then
-    writefile(Name, game:HttpGet("https://download2297.mediafire.com/sxcpuh83yxug/hwzamjpuf57kav8/Peter+Griffin+Says+The+N+Word.mp4"))
+    writefile(Name, game:HttpGet("https://github.com/cy4nkidd/scripts/blob/main/Peter%20Griffin%20Says%20The%20N%20Word.mp4?raw=true"))
 end
 
 video.Video = syn and getsynasset(Name) or getcustomasset(Name)
